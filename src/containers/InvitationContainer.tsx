4a3259888f720f9147fc854e552e4e1008fea01f
joi0104 <jjjjoi0104@gmail.com>
2020-09-02 00:46:29 +0900

    feat: #66. 초대장 로딩 대기

--- src/containers/InvitationContainer.tsx
+++ src/containers/InvitationContainer.tsx
@@ -1,10 +1,11 @@
 /* External dependencies */
-import React, { useEffect } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import _ from 'lodash'
 
 /* Internal dependencies */
+import LoadingAnimation from 'components/LoadingAnimation'
 import Invitation from 'components/Invitation'
 import * as invitationAction from 'redux/reducers/invitationReducer'
 import * as invitationSelector from 'redux/selectors/invitationSelector'
@@ -14,22 +15,36 @@ interface InvitationContainerProps {
 }
 
 function InvitationContainer({ invitationId }: InvitationContainerProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [isFetching, setIsFetching] = useState<boolean>(true)
+  const LOADING_TIME = 3000
   const dispatch = useDispatch()
   const history = useHistory()
   const invitation = useSelector(invitationSelector.getInvitation)
 
-  useEffect(() => {
-    ;(async () => {
-      try {
-        await dispatch(invitationAction.getInvitation({ invitationId })).promise
-      } catch (error) {
-        const errorStatusCode = _.get(error, ['response', 'status'])
-        history.replace(history.location.pathname, { errorStatusCode })
-      }
-    })()
+  const loadInvitatoin = () => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, LOADING_TIME)
+  }
+
+  const fetchInvitation = useCallback(async () => {
+    try {
+      await dispatch(invitationAction.getInvitation({ invitationId }))
+    } catch (error) {
+      const errorStatusCode = _.get(error, ['response', 'status'])
+      history.replace(history.location.pathname, { errorStatusCode })
+    } finally {
+      setIsFetching(false)
+    }
   }, [invitationId, history, dispatch])
 
-  return <Invitation invitation={invitation} />
+  useEffect(() => {
+    loadInvitatoin()
+    fetchInvitation()
+  }, [fetchInvitation])
+
+  return isLoading || isFetching ? <LoadingAnimation /> : <Invitation invitation={invitation} />
 }
 
 export default InvitationContainer
