1c0027307323f71471a6ffaa5cc695323f3ba464
joi0104 <jjjjoi0104@gmail.com>
2020-10-10 02:07:43 +0900

    feat: #107,#23 댓글기능 추가

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -1,27 +1,79 @@
 /* External dependencies */
-import React from 'react'
-import { OrderedMap } from 'immutable'
+import React, { useState } from 'react'
+import Immutable from 'immutable'
 import classNames from 'classnames/bind'
 
 /* Internal dependencies */
 import Comment from 'components/Comment'
 import CommentModel from 'models/Comment'
 import styles from './CommentList.module.scss'
+import TextUnderline from 'elements/TextUnderline'
+import WithNewline from 'hocs/WithNewline'
 
 interface commentListProps {
-  comments: OrderedMap<number, CommentModel>
+  comments: Immutable.List<CommentModel>
+  mainImage: string
+  contents: string
+  createComment: Function
 }
 
 const cx = classNames.bind(styles)
 
-function CommentList({ comments }: commentListProps) {
+function CommentList({ comments, mainImage, contents, createComment }: commentListProps) {
+  const [form, setForm] = useState({
+    userName: '',
+    content: '',
+  })
+
+  const initForm = () => {
+    setForm({
+      userName: '',
+      content: '',
+    })
+  }
+
+  const onChage = e => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const onSubmit = e => {
+    e.preventDefault()
+    createComment(form)
+    initForm()
+  }
+
   return (
-    <div className={cx('comment-list')}>
-      <ul className={cx('comments')}>
-        {comments.toList().map(comment => (
-          <Comment key={comment.id} comment={comment} />
-        ))}
-      </ul>
+    <div className={cx('comment-list-wrapper')}>
+      <header>
+        <div className={cx('character-wrapper')}>
+          <img src={mainImage} alt="" />
+        </div>
+      </header>
+      <section>
+        <article className={cx('comment-list-description')}>
+          <TextUnderline className={cx('title')}>댓글을 입력해주세요.</TextUnderline>
+          <p className={cx('description')}>
+            <WithNewline>{contents}</WithNewline>
+          </p>
+        </article>
+        <article className={cx('comment-list-content')}>
+          <ul>
+            {comments.toList().map(comment => (
+              <Comment key={comment.id} comment={comment} />
+            ))}
+          </ul>
+        </article>
+      </section>
+      <div className={cx('new-comment')} onSubmit={onSubmit}>
+        <form>
+          <input type="text" name="userName" value={form.userName} placeholder="이름을 입력해주세요" onChange={onChage} />
+          <input type="text" name="content" value={form.content} placeholder="댓글을 입력해주세요" onChange={onChage} />
+          <button type="submit">댓글 입력하기</button>
+        </form>
+      </div>
     </div>
   )
 }
